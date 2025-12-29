6df2897fd50b30b4a9525577688454a435dedc99
Yingkai Niu <nicholas.t.e838@gmail.com>
2025-12-29 18:16:57 +0800

    1

--- manim/lecture1/my_first_ manim_animation.py
+++ manim/lecture1/my_first_ manim_animation.py
@@ -2,7 +2,7 @@ from manim import *# 调用manim库中的所有模块
 class MyFirstManimAnimation(Scene):
     def construct(self):
         self.wait(1)#等待一秒
-        hello_manim = Text("hello, Manim!").scale(3)
+        hello_manim = Tex("hello, Manim!").scale(3)
 
         self.play(Write(hello_manim))#书写 hello world
         self.wait(2)
