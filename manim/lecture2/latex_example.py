0cfb8479afac1e59344ab12291b663360e86c2ee
Yingkai Niu <nicholas.t.e838@gmail.com>
2025-12-30 21:29:19 +0800

    1

--- manim/lecture2/latex_example.py
+++ manim/lecture2/latex_example.py
@@ -1,5 +1,21 @@
 from manim import *
-class LatexExample(Scene):
+class EulerFormula(Scene):
     def construct(self):
         text=MathTex(r"e^{i\pi} + 1 = 0")
         self.add(text)
+
+
+class NavierStokesEquation(Scene):
+    def construct(self):
+        text=MathTex(r"\nabla \cdot (\rho \mathbf{u} \cdot \nabla \mathbf{u}) = 0")
+        self.add(text)
+
+class LagrangianEquation(Scene):
+    def construct(self):
+        text=MathTex(r"\frac{d}{dt} \int_{\Omega} \rho \mathbf{u} \cdot \mathbf{u} \, dV = 0")
+        self.add(text)
+
+class Matrix(Scene):
+    def construct(self):
+        matrix = Matrix([[1,2,3],[4,5,6],[7,8,9]])
+        self.add(matrix)
