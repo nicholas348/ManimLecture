01f5649ad1c85313bb8a07b74dc8f8f9ae1bb602
Tagin T <109495720+524163893@users.noreply.github.com>
2025-12-30 23:57:48 +0800

    Delete python directory

--- python/lecture2/type_hint.py
+++
@@ -1,7 +0,0 @@
-def greet(name: str, age: int) -> str:
-    return f"Hello {name}, you are {age} years old."
-
-def process_data(data: list[int] | float) -> float:
-    if isinstance(data, list):
-        return sum(data) / len(data)
-    return data * 1.0
--- python/lecture3/error_raising_example.py
+++
@@ -1,7 +0,0 @@
-def series(x):
-    try:
-        return 1/(1-x)
-    except x<=0 or x>=1:
-        raise ValueError("x must be between 0 and 1")
-    finally:
-        print("Calculation completed")
