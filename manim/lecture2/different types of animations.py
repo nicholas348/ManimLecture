ce323442d09b76582e3732770ee32f70821947c4
Yingkai Niu <nicholas.t.e838@gmail.com>
2025-12-30 13:32:25 +0800

    1

---
+++ manim/lecture2/different types of animations.py
@@ -0,0 +1,21 @@
+from manim import *
+class AnimateSyntax(Scene):
+    def construct(self):
+        s = Square(color=GREEN, fill_opacity=0.5)
+        c = Circle(color=RED, fill_opacity=0.5)
+        self.add(s, c)
+        self.play(s.animate.shift(UP), c.animate.shift(DOWN))
+        self.play(VGroup(s, c).animate.arrange(RIGHT))
+        self.play(c.animate(rate_func=linear).shift(RIGHT).scale(2))
+        self.wait()
+
+
+class AnimateProblem(Scene):
+    def construct(self):
+        left_square = Square()
+        right_square = Square()
+        VGroup(left_square, right_square).arrange(RIGHT, buff=1)
+        self.add(left_square, right_square)
+        self.play(left_square.animate.rotate(PI), Rotate(right_square, PI), run_time=2)
+
+
