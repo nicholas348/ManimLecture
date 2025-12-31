fb24a82454bf079187aec9081006493808982fa0
Yingkai Niu <nicholas.t.e838@gmail.com>
2025-12-31 17:10:06 +0800

    changed the comment and structure in my_first_ manim_animation.py

--- manim/lecture1/my_first_ manim_animation.py
+++ manim/lecture1/my_first_ manim_animation.py
@@ -1,10 +1,14 @@
 from manim import *# 调用manim库中的所有模块
 class MyFirstManimAnimation(Scene):
     def construct(self):
-        self.wait(1)#等待一秒
-        hello_manim = Tex("hello, Manim!").scale(3)
 
-        self.add(hello_manim)#书写 hello world
+        # 等待一秒
+        self.wait(1)
+        hello_manim = (Tex("hello, Manim!")#创造一个Manim文字对象
+                       .scale(3))#放大3倍
+
+        # 书写 hello world
+        self.add(hello_manim)
 
 
 """渲染：manim -qm -p (--renderer=opengl) my_first_ manim_animation.py MyFirstManimAnimation"""
