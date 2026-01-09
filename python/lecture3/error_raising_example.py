fb7b27c6bda74592f1fb658ef9092cf70fcc19e1
Yingkai Niu <nicholas.t.e838@gmail.com>
2026-01-09 13:39:07 +0800

    added class_example.ipynb

--- python/lecture3/error_raising_example.py
+++ python/lecture3/error_raising_example.py
@@ -1,12 +1,13 @@
 def series(x):
     # 尝试执行程序
+    #1+x+x^2+...=1/(1-x)
     try:
 
         #这里返回代码
         return 1/(1-x)
 
     # 有意外情况
-    except x<=0 or x>=1:
+    except x<=-1 or x>=1:
 
         # 返回错误
         raise ValueError("x must be between 0 and 1")
